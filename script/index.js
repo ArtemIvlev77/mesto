a05d1c1907836872a9362b55ed3f0b218e593232
ArtemIvlev77 <artemivlev@yandex.ru>
2020-09-28 17:57:38 +0300

    feat: +overlay + popup conteiner
    Добавлен оверлей на страницу при активном модальном окне и сверстан внешний вид для модального окна

---
+++ script/index.js
@@ -0,0 +1,8 @@
+const buttonOpenPopup = document.querySelector(".profile__edit-btn");
+const buttonClosePopuo = document.querySelector(".popup__close-btn");
+const Popup = document.querySelector(".popup");
+
+
+const popupToggle = () => {
+  popup.classList.toggle("popup_is-opened");
+}
