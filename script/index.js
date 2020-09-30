6817c43491b243a6e03255e5d17b7698ee0fc3c3
ArtemIvlev77 <artemivlev@yandex.ru>
2020-09-30 15:43:57 +0300

    feat: форма функционирует

--- script/index.js
+++ script/index.js
@@ -1,8 +1,36 @@
-const buttonOpenPopup = document.querySelector(".profile__edit-btn");
-const buttonClosePopuo = document.querySelector(".popup__close-btn");
-const Popup = document.querySelector(".popup");
+const buttonOpenForm = document.querySelector(".profile__edit-btn");
+const buttonCloseForm = document.querySelector(".form__close-btn");
+const form = document.querySelector(".form");
 
 
-const popupToggle = () => {
-  popup.classList.toggle("popup_is-opened");
+const formToggle = (event) => {
+  form.classList.toggle("form_is-opened");
+
+};
+
+buttonOpenForm.addEventListener("click", formToggle);
+buttonCloseForm.addEventListener("click", formToggle);
+
+
+
+let formElement = document.querySelector('.form__container'); // Воспользуйтесь методом querySelector()
+let nameInput = document.querySelector('.form__text_name'); // Воспользуйтесь инструментом .querySelector()
+let jobInput = document.querySelector('.form__text_discription'); // Воспользуйтесь 
+
+ nameInput.value += document.querySelector('.profile__info_name').textContent;
+ jobInput.value += document.querySelector('.profile__info_discription').textContent;
+// Получите значение полей из свойства value
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  document.querySelector('.profile__info_name').textContent = nameInput.value;
+  document.querySelector('.profile__info_discription').textContent = jobInput.value;
+  formToggle();
 }
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
+
+console.log(nameInput.textContent, jobInput.textContent);
