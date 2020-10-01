9c0a3da23082ae9345b35ae5617f8f0119bd7c93
Artem.Ivlev <ArtemIvlev@yandex.ru>
2020-10-02 00:28:14 +0300

    fixes: закончил правки по ревью № 1
    Исправлены файлы по БЭМ, исправлена семантика разметки, исправлен script, исправлен адаптив

--- script/index.js
+++ script/index.js
@@ -2,33 +2,31 @@ const buttonOpenForm = document.querySelector(".profile__edit-btn");
 const buttonCloseForm = document.querySelector(".popup__close-btn");
 const popup = document.querySelector(".popup");
 
+const formElement = popup.querySelector('.popup__form');
+const nameInput = popup.querySelector('.popup__name-input');
+const jobInput = popup.querySelector('.popup__job-input');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
 
 const formToggle = () => {
   popup.classList.toggle("popup_is-opened");
-
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 };
+
+
+
 
 buttonOpenForm.addEventListener("click", formToggle);
 buttonCloseForm.addEventListener("click", formToggle);
 
-
-
-const formElement = popup.querySelector('.popup__container');
-const nameInput = popup.querySelector('.popup__name-imput');
-const jobInput = popup.querySelector('.popup__job-input');
-const profileName = popup.querySelector('.profile__name');
-const profileJob = popup.querySelector('.profile__job');
-
-nameInput.value += profileName.textContent;
-jobInput.value += profileJob.textContent;
-
 function formSubmitHandler(evt) {
-  evt.preventDefault();
+  evt.preventDefault();  
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
   formToggle();
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-popup.addEventListener('submit', formSubmitHandler);
+
+
+formElement.addEventListener('submit', formSubmitHandler);
